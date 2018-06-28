4d72c0a5e7fd03c9dd546f277add0a9559abea9a
rbenjani <benjani86@gmail.com>
2018-06-28 12:48:30 +0200

    Drag & Drop Tutorial

---
+++ Angular 2/todomvc-angular-4-drag-&-drop/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// Módulo para drag and drop
+import { DndModule } from 'ng2-dnd';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    DndModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
